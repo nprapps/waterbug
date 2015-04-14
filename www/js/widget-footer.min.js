36ae4cbf3b336b56c22af06deea0c8cfed4d8044
Tyler Fisher <tyler@tylerjfisher.com>
2015-04-14 13:17:40 -0400

    make gitignore work

--- www/js/widget-footer.min.js
+++
@@ -1,9 +0,0 @@
-/*
- * Looking for the full, uncompressed source? Try here:
- *
- * https://github.com/nprapps/$NEW_REPOSITORY_NAME
- *
- * The following files are included in this compressed version:
- *
- * www/js/widget.js
- */
