6c5edb419a2e4e1ef7a69ec1e9bc7fdd7b7a3e25
Tyler Fisher <tyler@tylerjfisher.com>
2015-04-15 17:28:59 -0400

    cleanup, comments, refactor

--- www/js/app.js
+++ www/js/app.js
@@ -1,20 +1,55 @@
+// DOM elements
 var $source;
 var $save;
 var $textColor;
 var $crop;
+var $imageLoader;
 var canvas;
-var imageLoader;
-var ctx;
-var imageFilename;
-var img = new Image();
-var logo = new Image();
+
+// state
 var scaledImageHeight;
 var fixedWidth = 1000;
 var dy = 0;
+var imageFilename;
 var currentCrop = 'original';
 var currentLogoColor = 'white';
 var currentTextColor = 'white';
 
+// JS objects
+var ctx;
+var img = new Image();
+var logo = new Image();
+
+
+var onDocumentLoad = function(e) {
+    $source = $('#source');
+    canvas = $('#imageCanvas')[0];
+    $imageLoader = $('#imageLoader');
+    ctx = canvas.getContext('2d');
+    $save = $('.save-btn');
+    $textColor = $('input[name="textColor"]');
+    $crop = $('input[name="crop"]');
+
+    img.src = 'assets/test.png';
+    img.onload = renderCanvas;
+    logo.onload = renderCanvas;
+
+    $source.on('keyup', renderCanvas);
+    $imageLoader.on('change', handleImage);
+    $save.on('click', onSaveClick);
+    $textColor.on('change', onTextColorChange);
+    $('input[name="logoColor"]').on('change', onLogoColorChange);
+    $crop.on('change', onCropChange);
+    $(canvas).on('mousedown', onDrag);
+
+    loadLogo();
+    renderCanvas();
+}
+
+
+/*
+* Take an image from file input and load it
+*/
 var handleImage = function(e) {
     var reader = new FileReader();
     reader.onload = function(e){
@@ -24,30 +59,37 @@ var handleImage = function(e) {
     reader.readAsDataURL(e.target.files[0]);
 }
 
+/*
+* Load the logo based on radio buttons
+*/
 var loadLogo = function() {
     logo.src = 'assets/npr-' + currentLogoColor + '.svg';
 }
 
+
+/*
+* Draw the image, then the logo, then the text
+*/
 var renderCanvas = function() {
+    // canvas is always the same width
     canvas.width = fixedWidth;
 
+    // if we're cropping, use the aspect ratio for the height
     if (currentCrop !== 'original') {
         canvas.height = fixedWidth / (16/9);
     }
+
+    // clear the canvas
     ctx.clearRect(0,0,canvas.width,canvas.height);
 
+    // determine height of canvas and scaled image, then draw the image
     var imageAspect = img.width / img.height;
     if (currentCrop === 'original') {
         canvas.height = fixedWidth / imageAspect;
         scaledImageHeight = canvas.height;
-    } else {
-        scaledImageHeight = fixedWidth / imageAspect;
-    }
-
-
-    if (currentCrop === 'original') {
         ctx.drawImage(img, 0, 0, fixedWidth, scaledImageHeight);
     } else {
+        scaledImageHeight = fixedWidth / imageAspect;
         ctx.drawImage(
             img,
             0,
@@ -61,13 +103,16 @@ var renderCanvas = function() {
         );
     }
 
+    // set alpha channel, draw the logo
     if (currentLogoColor === 'white') {
         ctx.globalAlpha = "0.8";
     }
-
     ctx.drawImage(logo, canvas.width - (150 + 40), 40, 150, 52);
 
+    // reset alpha channel so text is not translucent
     ctx.globalAlpha = "1";
+
+    // draw the text
     ctx.textBaseline = 'bottom';
     ctx.textAlign = 'left';
     ctx.fillStyle = currentTextColor;
@@ -83,6 +128,9 @@ var renderCanvas = function() {
     ctx.fillText($source.val(), 40, canvas.height - 40);
 }
 
+/*
+* Download the image on save click
+*/
 var onSaveClick = function() {
     /// create an "off-screen" anchor tag
     var link = document.createElement('a'),
@@ -112,6 +160,10 @@ var onSaveClick = function() {
     }
 }
 
+
+/*
+* Handle dragging the image for crops when applicable
+*/
 var onDrag = function(e) {
     e.preventDefault();
     var originY = e.clientY;
@@ -148,6 +200,9 @@ var onDrag = function(e) {
         });
 }
 
+/*
+* Handle logo radio button clicks
+*/
 var onLogoColorChange = function(e) {
     currentLogoColor = $(this).val();
 
@@ -155,13 +210,18 @@ var onLogoColorChange = function(e) {
     renderCanvas();
 }
 
+/*
+* Handle text color radio button clicks
+*/
 var onTextColorChange = function(e) {
     currentTextColor = $(this).val();
 
-    loadLogo();
     renderCanvas();
 }
 
+/*
+* Handle crop radio button clicks
+*/
 var onCropChange = function() {
     currentCrop = $(this).val();
 
@@ -174,27 +234,4 @@ var onCropChange = function() {
     renderCanvas();
 }
 
-$(document).ready(function() {
-    $source = $('#source');
-    canvas = $('#imageCanvas')[0];
-    imageLoader = $('#imageLoader');
-    ctx = canvas.getContext('2d');
-    $save = $('.save-btn');
-    $textColor = $('input[name="textColor"]');
-    $crop = $('input[name="crop"]');
-
-    img.src = 'assets/test.png';
-    img.onload = renderCanvas;
-    logo.onload = renderCanvas;
-
-    $source.on('keyup', renderCanvas);
-    imageLoader.on('change', handleImage);
-    $save.on('click', onSaveClick);
-    $textColor.on('change', onTextColorChange);
-    $('input[name="logoColor"]').on('change', onLogoColorChange);
-    $crop.on('change', onCropChange);
-    $(canvas).on('mousedown', onDrag);
-
-    loadLogo();
-    renderCanvas();
-});
+$(onDocumentLoad);
