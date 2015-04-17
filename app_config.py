e5b91c8c0a608c380c8c4ac806cc7580fe0c3ffb
Tyler Fisher <tyler@tylerjfisher.com>
2015-04-17 17:04:12 -0400

    change repo name

--- app_config.py
+++ app_config.py
@@ -25,7 +25,7 @@ PROJECT_SLUG = 'waterbug'
 PROJECT_FILENAME = 'waterbug'
 
 # The name of the repository containing the source
-REPOSITORY_NAME = 'watermarker'
+REPOSITORY_NAME = 'waterbug'
 GITHUB_USERNAME = 'nprapps'
 REPOSITORY_URL = 'git@github.com:%s/%s.git' % (GITHUB_USERNAME, REPOSITORY_NAME)
 REPOSITORY_ALT_URL = None # 'git@bitbucket.org:nprapps/%s.git' % REPOSITORY_NAME'
